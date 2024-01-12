0b72440c129d89c08a0639a809c85e19561a80b7
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-01-12 19:05:45 +0000

    Created scores.js file

---
+++ scores.js
@@ -0,0 +1,10 @@
+// scores.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Code to run when the DOM is ready
+    // You can implement functions to display and clear highscores here
+    // For example:
+    // function displayHighscores() { ... }
+    // function clearHighscores() { ... }
+  });
+  
